783480ea64f117ffb66fe513599ba84a9b0673fe
Ryan Williams <rgwilliams@hotmail.ca>
2018-03-11 23:33:58 -0400

    Replaced sessions with passport and JWT auth

--- app.js
+++ app.js
@@ -1,31 +1,23 @@
 const express = require('express');
-const session = require('express-session');
-const PgSession = require('connect-pg-simple')(session);
 const path = require('path');
 const bodyParser = require('body-parser');
+const passport = require('passport');
+const jwtStrategy = require('./config/strategy');
 const mountRoutes = require('./routes');
-const { pool } = require('./db');
 // const favicon = require('serve-favicon');
 
+passport.use(jwtStrategy);
+
 const app = express();
-mountRoutes(app);
+// react client to build to public and be served statically
+app.use(express.static(path.join(__dirname, 'public')));
 // uncomment after placing your favicon in /public
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(passport.initialize());
 
-app.use(session({
-  store: new PgSession({ pool }),
-  secret: process.env.SECRET,
-  resave: false,
-  saveUninitialized: false,
-  cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 },
-}));
-
-// react client to build to public and be served statically
-app.use(express.static(path.join(__dirname, 'public')));
-
-// app.use('/', routes);
+mountRoutes(app);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
---
+++ config/strategy.js
@@ -0,0 +1,26 @@
+const { Strategy: JwtStrategy, ExtractJwt } = require('passport-jwt');
+const { JWTKEY } = require('./config');
+const db = require('../db');
+
+const jwtOpts = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: JWTKEY,
+};
+
+const strategy = new JwtStrategy(jwtOpts, async (jwtPayload, done) => {
+  const { id } = jwtPayload;
+  try {
+    const { rows } = await db.query('SELECT user_id, name, email, title, deactivated, permission_level_id FROM users WHERE user_id = $1', [id]);
+    console.log(rows);
+    const user = rows[0];
+    if (user) {
+      done(null, user);
+    } else {
+      done(null, false);
+    }
+  } catch (err) {
+    done(err, false);
+  }
+});
+
+module.exports = strategy;
