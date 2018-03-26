7159e6d1522409f7f3c00039d51b04f8ca668d86
Ryan Williams <rgwilliams@hotmail.ca>
2018-03-25 23:53:30 -0400

    updated get auditid and added logic to handle res based on if completed

--- routes/audits.js
+++ routes/audits.js
@@ -1,4 +1,5 @@
 const router = require('express-promise-router')();
+const { body, validationResult, oneOf } = require('express-validator/check');
 const db = require('../db');
 
 // get all audits assigned to the requesting user
@@ -16,10 +17,24 @@ router.get('/reviews', async (req, res) => {
 });
 
 // get specific audit by id
-router.get('/:auditid', async (req, res) => {
+// if audit is not completed also send active issues to render form
+router.get('/:auditid', async (req, res, next) => {
   const { auditid } = req.params;
   const { rows } = await db.query('SELECT * FROM audits WHERE audit_id = $1', [auditid]);
-  res.json(rows[0]);
+  const audit = rows[0];
+  // if the audit has already been completed send it
+  if (audit.is_complete === true) return res.json(audit);
+  // else provide the active issues alongside audit so the user can complete
+  res.locals.audit = audit;
+  return next();
+}, async (req, res) => {
+  const { rows } = await db.query('SELECT issue_id, issue_type FROM audit_issue_values WHERE deactivated = false');
+  const issues = rows;
+  const { audit } = res.locals;
+  res.json({ audit, issues });
+});
+
+// manual audit creation
 });
 
 module.exports = router;
