54c80a00939fcc27d513b55314c50cbffefbc00d
Ryan Williams <rgwilliams@hotmail.ca>
2018-04-29 12:38:38 -0400

    removed favicon, now provided through react

--- app.js
+++ app.js
@@ -4,15 +4,14 @@ const bodyParser = require('body-parser');
 const passport = require('passport');
 const jwtStrategy = require('./config/strategy');
 const mountRoutes = require('./routes');
-// const favicon = require('serve-favicon');
 
 passport.use(jwtStrategy);
 
 const app = express();
-// react client to build to public and be served statically
-app.use(express.static(path.join(__dirname, 'public')));
-// uncomment after placing your favicon in /public
-// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+// react client to build to client/public and be served statically
+app.use(express.static(path.join(__dirname, 'client', 'public')));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(passport.initialize());
@@ -28,12 +27,9 @@ app.use((req, res, next) => {
 
 app.use((err, req, res, next) => {
   res.status(err.status || 500);
-  // expose entire err obj in development
-  if (app.get('env') === 'development') {
-    res.json(err);
-  } else {
-    res.json({ error: err.message });
-  }
+  // print stack trace in development
+  if (app.get('env') === 'development') console.error(err);
+  res.json({ error: err.message });
 });
 
 module.exports = app;
