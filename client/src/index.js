4edf02e011f753d96743169e307ed970dadd11d6
Ryan Williams <rgwilliams@hotmail.ca>
2018-06-03 17:49:04 -0400

    use react router

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './index.css';
-import App from './App';
+import LoginPage from './components/LoginPage/LoginPage';
+import Dashboard from './components/Dashboard/Dashboard';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+    <div>
+      <Route exact path="/" component={LoginPage} />
+      <Route path="/dashboard" component={Dashboard} />
+    </div>
+  </Router>,
+  document.getElementById('root'),
+);
+
 registerServiceWorker();
